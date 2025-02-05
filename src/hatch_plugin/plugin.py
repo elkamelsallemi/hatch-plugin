068dba537434c1be70d016673f7ded2a56816ccf
elkamelsallemi <elkamel.sallemi@gmail.com>
2025-02-05 19:41:50 +0100

    Update project

--- src/hatch_plugin/plugin.py
+++ src/hatch_plugin/plugin.py
@@ -10,17 +10,16 @@ from python_minifier import minify
 class MinifyBuildHook(BuildHookInterface):
     PLUGIN_NAME = "minifyer"
 
-    # def __init__(self, *args: Any, **kwargs: Any) -> None:
-    #     super().__init__(*args, **kwargs)
-    #     self.builder = self.build_config.builder
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.builder = self.build_config.builder
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         """Called during the initialization phase of the build"""
+        project_name = self.metadata.name
+        print(f"Hello, World! Initialization phase. Project name: {project_name}")
         print("Hello, World! Initialization phase.")
-        project_name = self.build_config.name
-        
-        # Print the project name for debugging
-        print(f"Project name must start with 'monorepo-', but got: {project_name}")
+
 
     def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:  # noqa: ARG002
         """Called during the finalization phase of the build"""
