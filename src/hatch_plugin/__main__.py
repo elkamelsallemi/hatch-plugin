06746112384915da4d138a0b55ffc37eb57e54d9
elkamelsallemi <elkamel.sallemi@gmail.com>
2025-02-05 15:59:11 +0100

    Update project

---
+++ src/hatch_plugin/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present elkamelsallemi <elkamel.sallemi@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+import sys
+
+if __name__ == "__main__":
+    from hatch_plugin.cli import hatch_new_cli
+
+    sys.exit(hatch_new_cli())
---
+++ src/hatch_plugin/cli/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2025-present elkamelsallemi <elkamel.sallemi@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+import click
+import subprocess
+from hatch_plugin.__about__ import __version__
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.version_option(version=__version__, prog_name="monorepo")
+def hatch_new_cli():
+    """Build the project using Hatch."""
+    result = subprocess.run(['hatch', 'build'], capture_output=True, text=True)
+    if result.returncode == 0:
+        click.echo("Build successful.")
+    else:
+        click.echo(f"Build failed with error: {result.stderr}")
+        exit(result.returncode)
