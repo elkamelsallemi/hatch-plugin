72377b0a3a13cf65f97d861329bc327c5e2fec0a
elkamelsallemi <elkamel.sallemi@gmail.com>
2025-02-05 19:04:56 +0100

    Update project

--- src/hatch_plugin/plugin.py
+++ src/hatch_plugin/plugin.py
@@ -6,7 +6,6 @@ from typing import Any
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from python_minifier import minify
-import argparse
 
 class MinifyBuildHook(BuildHookInterface):
     PLUGIN_NAME = "minifyer"
@@ -14,39 +13,11 @@ class MinifyBuildHook(BuildHookInterface):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.builder = self.build_config.builder
-        self.minified_directory = TemporaryDirectory()
-
-    
-    def get_cli_path(self) -> Path:
-        """Get the path from the CLI arguments."""
-        parser = argparse.ArgumentParser(description="Minify Python files.")
-        parser.add_argument(
-            '--source-path', type=Path, required=True, help="Path to the source directory"
-        )
-        args = parser.parse_args()
-        return args.source_path   
-
-    def minify_included_files(self, build_data):
-        self.app.display_waiting("Minifying python source... hhh")
-        for included_file in self.builder.recurse_included_files():
-            if not included_file.path.endswith(".py") or not included_file.distribution_path:
-                self.app.display_debug(f"Not minified: {included_file.path}")
-                continue
-            source_path = Path(included_file.path)
-            minified_path = Path(self.minified_directory.name) / included_file.distribution_path
-            minified_path.parent.mkdir(parents=True, exist_ok=True)
-            minified = minify(source_path.read_text(encoding="utf-8"), source_path.name)
-            minified_path.write_text(minified, encoding="utf-8")
-            build_data["force_include"][minified_path] = included_file.distribution_path
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        if version != "standard":
-            return
-        
-        source_path = self.get_cli_path()  # Read the source path from CLI argument
-        self.minify_included_files(build_data)
+        """Called during the initialization phase of the build"""
+        print("Hello, World! Initialization phase.")
 
     def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:  # noqa: ARG002
-        if version != "standard":
-            return
-        self.minified_directory.cleanup()
+        """Called during the finalization phase of the build"""
+        print("Hello, World! Finalization phase.")
