5f17739fa5e1c7f8a3659976aa5569f6bdcc1d61
elkamelsallemi <elkamel.sallemi@gmail.com>
2025-02-05 19:27:04 +0100

    Update project

--- src/hatch_plugin/plugin.py
+++ src/hatch_plugin/plugin.py
@@ -17,17 +17,9 @@ class MinifyBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         """Called during the initialization phase of the build"""
         print("Hello, World! Initialization phase.")
-        files_to_exclude = ["__about__.py"]  # Specify files to exclude
-        excluded_files = set()
-
-        for included_file in build_data.get("force_include", {}).copy():
-            if any(included_file.endswith(f) for f in files_to_exclude):
-                print(f"Excluding file: {included_file}")
-                excluded_files.add(included_file)
-
-        # Remove the excluded files from the "force_include" list
-        for excluded_file in excluded_files:
-            build_data["force_include"].pop(excluded_file, None)
+        project_name = build_data.get("name", "")
+        if not project_name.startswith("monorepo-"):
+            raise ValueError(f"Project name must start with 'monorepo-', but got: {project_name}")
 
     def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:  # noqa: ARG002
         """Called during the finalization phase of the build"""
